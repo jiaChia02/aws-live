8338c678fa462418339055a4b79ab9069486ca6b
THENG CHIA CHIA <127763561+jiaChia02@users.noreply.github.com>
2023-08-28 11:50:05 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,6 @@
+customhost = "database-1.cybcizvu0ofd.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "thengchiachia-employee"
+customregion = "us-east-1"
